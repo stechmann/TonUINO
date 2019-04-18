35f17ad5ba6fd477835d66fe3547e6ba82d0aae3
Til Schneider <github@murfman.de>
2019-04-18 17:24:02 +0200

    adds support for generating messages using Amazon Polly

--- tools/create_audio_messages.py
+++ tools/create_audio_messages.py
@@ -1,94 +1,55 @@
 #!/usr/bin/python
 
-# By default the text-to-speech engine of MacOS is used (command `say`)
-# If you want to use the Google text-to-speech engine instead, go to https://cloud.google.com/text-to-speech/
-# and create an account and an API key.
-# Then uncomment the following line and put in your API key.
-#googleApiKey="ABCDEFGH12345678"
+# Creates the audio messages needed by TonUINO.
 
 
-### End of config ###
+import argparse, os, re, shutil, sys, text_to_speach
 
 
-import base64, json, os, re, shutil, subprocess, sys
+if __name__ == '__main__':
+    argFormatter = lambda prog: argparse.RawDescriptionHelpFormatter(prog, max_help_position=30, width=100)
+    argparser = text_to_speach.PatchedArgumentParser(
+        description=
+            'Creates the audio messages needed by TonUINO.\n\n' +
+            text_to_speach.textToSpeechDescription,
+        usage='%(prog)s [optional arguments...]',
+        formatter_class=argFormatter)
+    argparser.add_argument('-i', '--input', type=str, default='.', help='The directory where `audio_messages_*.txt` files are located.')
+    argparser.add_argument('-o', '--output', type=str, default='sd-card', help='The directory where to create the audio messages.')
+    text_to_speach.addArgumentsToArgparser(argparser)
+    argparser.add_argument('--skip-numbers', action='store_true', help='If set, no number messages will be generated (`0001.mp3` - `0255.mp3`)')
+    args = argparser.parse_args()
 
 
-if len(sys.argv) != 2:
-    print("Usage: " + sys.argv[0] + " [en | de]")
-    exit(1)
+    text_to_speach.checkArgs(argparser, args)
 
-lang = sys.argv[1]
-if lang == 'de':
-    sayVoice = 'Anna'
-    googleVoice = { 'languageCode': 'de-DE', 'name': 'de-DE-Wavenet-C' }
-elif lang == 'en':
-    sayVoice = 'Samantha'
-    googleVoice = { 'languageCode': 'en-US', 'name': 'en-US-Wavenet-D' }
-else:
-    print("Unsupported language: " + lang)
-    exit(1)
+    audioMessagesFile = '{}/audio_messages_{}.txt'.format(args.input, args.lang)
+    if not os.path.isfile(audioMessagesFile):
+        print('Input file does not exist: ' + os.path.abspath(audioMessagesFile))
+        exit(1)
 
-targetDir = 'sd-card'
-if os.path.isdir(targetDir):
-    print("Directory `" + targetDir + "` already exists.")
-    exit(1)
-else:
-    os.mkdir(targetDir)
-    os.mkdir(targetDir + '/advert')
-    os.mkdir(targetDir + '/mp3')
-
-
-useGoogleTextToSpeech = True
-try:
-    googleApiKey
-except NameError:
-    useGoogleTextToSpeech = False
-
-
-def postJson(url, postBody):
-    response = subprocess.check_output(['curl', '--header', 'Content-Type: application/json; charset=utf-8', '--data', json.dumps(postBody).encode('utf-8'), url])
-    return json.loads(response)
-
-
-def textToSpeech(text, targetFile):
-    print('\nGenerating: ' + targetFile + ' - ' + text)
-    if useGoogleTextToSpeech:
-        responseJson = postJson(
-            'https://texttospeech.googleapis.com/v1beta1/text:synthesize?key=' + googleApiKey,
-            {
-                'audioConfig': {
-                    'audioEncoding': 'MP3',
-                    'speakingRate': 1.0,
-                    'pitch': 2.0,  # Default is 0.0
-                    'sampleRateHertz': 44100,
-                    'effectsProfileId': [ 'small-bluetooth-speaker-class-device' ]
-                },
-                'voice': googleVoice,
-                'input': { 'text': text }
-            }
-        )
-
-        mp3Data = base64.b64decode(responseJson['audioContent'])
-
-        with open(targetFile, 'wb') as f:
-            f.write(mp3Data)
+    targetDir = args.output
+    if os.path.isdir(targetDir):
+        print("Directory `" + targetDir + "` already exists.")
+        exit(1)
     else:
-        subprocess.call([ 'say', '-v', sayVoice, '-o', 'temp.aiff', text ])
-        subprocess.call([ 'ffmpeg', '-y', '-i', 'temp.aiff', '-acodec', 'libmp3lame', '-ab', '128k', '-ac', '1', targetFile ])
-        os.remove('temp.aiff')
+        os.mkdir(targetDir)
+        os.mkdir(targetDir + '/advert')
+        os.mkdir(targetDir + '/mp3')
 
 
-for i in range(1,256):
-    targetFile1 = '{}/mp3/{:0>4}.mp3'.format(targetDir, i)
-    targetFile2 = '{}/advert/{:0>4}.mp3'.format(targetDir, i)
-    textToSpeech('{}'.format(i), targetFile1)
-    shutil.copy(targetFile1, targetFile2)
+    if not args.skip_numbers:
+        for i in range(1,256):
+            targetFile1 = '{}/mp3/{:0>4}.mp3'.format(targetDir, i)
+            targetFile2 = '{}/advert/{:0>4}.mp3'.format(targetDir, i)
+            text_to_speach.textToSpeechUsingArgs(text='{}'.format(i), targetFile=targetFile1, args=args)
+            shutil.copy(targetFile1, targetFile2)
 
-with open('audio_messages_' + lang + '.txt') as f:
-    lineRe = re.compile('^([^|]+)\\|(.*)$')
-    for line in f:
-        match = lineRe.match(line.strip())
-        if match:
-            fileName = match.group(1)
-            text = match.group(2)
-            textToSpeech(text, targetDir + '/mp3/' + fileName)
+    with open(audioMessagesFile) as f:
+        lineRe = re.compile('^([^|]+)\\|(.*)$')
+        for line in f:
+            match = lineRe.match(line.strip())
+            if match:
+                fileName = match.group(1)
+                text = match.group(2)
+                text_to_speach.textToSpeechUsingArgs(text=text, targetFile=targetDir + '/mp3/' + fileName, args=args)
